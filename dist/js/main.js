40166144e248292244d6cf6e339a439a4c08174c
Maryna Kutsarenko <m.kutsarenko@gmail.com>
2021-06-04 20:14:28 +0300

    added few fieles

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,8 +1,5 @@
-
+alert('hi');
 let checkbox = document.querySelector('.filter-checkbox');
-let blockEfficiency = document.querySelector('.block_efficiency');
-let blockInformation = document.querySelector('.block_information');
-let blockTime = document.querySelector('.block_time');
 
 checkbox.addEventListener('change', function () {
    !checkbox.checked ? blockEfficiency.style.display = 'none' : blockEfficiency.style.display = 'block';
