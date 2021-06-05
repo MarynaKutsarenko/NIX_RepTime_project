50fe3096aeec17bd31e32346417bc8ee4412d1cf
Maryna Kutsarenko <m.kutsarenko@gmail.com>
2021-06-05 21:05:01 +0300

    chenges with burger

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,3 +3,11 @@
 // checkbox.addEventListener('change', function () {
 //    !checkbox.checked ? blockEfficiency.style.display = 'none' : blockEfficiency.style.display = 'block';
 // })
+
+(function () {
+   const menu_burger = document.querySelector('.menu_burger');
+
+   menu_burger.addEventListener('click', () => {
+      menu_burger.classList.toggle('menu_burger-active');
+      })
+}());
