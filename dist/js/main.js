ace466a4154302894db448f77d3fa4dff40447f3
Maryna Kutsarenko <m.kutsarenko@gmail.com>
2021-06-05 23:42:47 +0300

    done with nav

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,13 +1,21 @@
+(function () {
+   const menuBurger = document.querySelector('.menu_burger');
+   const menuItems = document.querySelector('.menu_items');
+   const menuItem = document.querySelector('.menu_item');
+
+   menuBurger.addEventListener('click', function () {
+      if (this.classList.contains('menu_burger-active')) {
+         this.classList.remove('menu_burger-active');
+         menuItems.style.display = 'none';
+      } else {
+         this.classList.add('menu_burger-active');
+         menuItems.style.display = 'block';
+      }
+   })
+}());
+
 // let checkbox = document.querySelector('.filter-checkbox');
 
 // checkbox.addEventListener('change', function () {
 //    !checkbox.checked ? blockEfficiency.style.display = 'none' : blockEfficiency.style.display = 'block';
 // })
-
-(function () {
-   const menu_burger = document.querySelector('.menu_burger');
-
-   menu_burger.addEventListener('click', () => {
-      menu_burger.classList.toggle('menu_burger-active');
-      })
-}());
